2eae45fbbb5d04e34464b4bad3dda1dc69949f63
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2022-12-25 22:07:00 +0900

    Themes 50%

--- src/App.js
+++ src/App.js
@@ -1,57 +1,22 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-`;
-
-const rotaionAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius:0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius:0px;
-  }
-`;
-
-// 모든 Emoji에 적용됨
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotaionAnimation} 1s linear infinite;
-  // Box 내의 Emoji에만 영향을 미침
-  ${Emoji} {
-    &:hover {
-      font-size: 96px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">🥳</Emoji>
-      </Box>
-      {/** Box 밖에 Emoji가 있기 때문에 pseudo selector에 의해서 선택되지 않음 */}
-      <Emoji>🥳</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
