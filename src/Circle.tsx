cd43d9f88b67cff1b1e02abd7478946c322afe05
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-01-02 17:08:43 +0900

    Typescript: Optional Props

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,18 +1,34 @@
 import styled from "styled-components";
 
-interface CircleProps {
+interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
-const Container = styled.div<CircleProps>`
+const Container = styled.div<ContainerProps>`
   width: 200px;
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+// ?를 이용해 required가 아닌 optional로 설정
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+  text?: string;
+}
+
+// borderColor는 optional이라 있을 수도 있고 없을 수도 있음
+// text = "default text": 노말 자바스크립트 구문으로 default value를 준 것
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  // ??를 이용해 없을 때 default값을 설정해줌, borderColor가 없다면 bgColor로 설정해줌
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
 }
 
 /* function Circle(props: CircleProps) {
