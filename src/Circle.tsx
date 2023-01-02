e752754a01552181c8365e22d4153a87e15dc207
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-01-02 16:17:56 +0900

    Typescript: Interface

---
+++ src/Circle.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+interface CircleProps {
+  bgColor: string;
+}
+
+const Container = styled.div<CircleProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+`;
+
+function Circle({ bgColor }: CircleProps) {
+  return <Container bgColor={bgColor} />;
+}
+
+/* function Circle(props: CircleProps) {
+  return <Container bgColor={props.bgColor} />;
+} */
+
+export default Circle;
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// dark모드와 light모드를 구분해서 적용하도록 theme을 정함
-import { ThemeProvider } from "styled-components";
 import App from "./App";
-
-// 반드시 theme 내부의 property 이름이 똑같게 해야함
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "#111",
-};
-
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke",
-};
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <ThemeProvider theme={darkTheme}>
-    <App />
-  </ThemeProvider>
-);
+root.render(<App />);
