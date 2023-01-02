7885531dc81e17d5268d5ae59a5dffaf359af339
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-01-02 18:43:24 +0900

    Typescript: Theme 100%

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,19 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-  // typescript: form의 type을 읽어와서 자동으로 string으로 타입 설정
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  // typescript를 이용해 event를 보호하는 것, 어떤 이벤트를 받을 지 결정해놓는 것
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("Hello", value);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { lightTheme } from "./theme";
+import { darkTheme, lightTheme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   // <themeProvider>는 styled-components의 컴포넌트 중 하나
-  <ThemeProvider theme={lightTheme}>
+  <ThemeProvider theme={darkTheme}>
     <App />
   </ThemeProvider>
 );
