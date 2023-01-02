1165ef6ff3289ab2828bdc3f904d4e30075ddce4
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-01-02 15:38:52 +0900

    Typescript Setting

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,9 @@ const lightTheme = {
   backgroundColor: "whitesmoke",
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <ThemeProvider theme={darkTheme}>
     <App />
