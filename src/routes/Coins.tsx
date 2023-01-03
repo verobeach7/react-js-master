76541ac132a4c86aae46ed5f415b2d83c72d2052
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-01-03 17:28:18 +0900

    Coin Tracker: Home

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,12 +1,92 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  margin-bottom: 10px;
+  border-radius: 15px;
+  a {
+    padding: 20px;
+    transition: color 0.2s ease-in-out;
+    display: block;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
 const Title = styled.h1`
+  font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
 
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
 // Coins 컴포넌트
 function Coins() {
-  return <Title>Coins</Title>;
+  // typescript에게 coins state가 CoinInterface 형식(type)이라고 알려주기
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  // useState<{loading:boolean}>({loading:true})로 typescript에 형식 안려줄 수 있음
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      {loading ? (
+        <Loader>"Loading..."</Loader>
+      ) : (
+        <CoinList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+            </Coin>
+          ))}
+        </CoinList>
+      )}
+    </Container>
+  );
 }
 
 export default Coins;
