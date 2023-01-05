8459a0b96ff9227e0c7bb490ba83aa3b8ef39cde
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-01-05 21:32:06 +0900

    Coin Tracker: Price Chart Part2(Apex Chart)

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,15 +1,70 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface IHistoricalData {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
 
 interface ChartProps {
   coinId: string;
 }
 
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
-    fetchCoinHistory(coinId)
+  const { isLoading, data } = useQuery<IHistoricalData[]>(
+    ["ohlcv", coinId],
+    () => fetchCoinHistory(coinId)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => Number(price.close)) as number[],
+            },
+          ]}
+          options={{
+            theme: { mode: "dark" },
+            chart: {
+              height: 500,
+              width: 500,
+              toolbar: { show: false },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: { curve: "smooth", width: 4 },
+            yaxis: { show: false },
+            xaxis: {
+              type: "datetime",
+              labels: { show: false },
+              categories: data?.map((price) =>
+                new Date(price.time_close * 1000).toISOString()
+              ),
+              axisTicks: { show: false },
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+            },
+            colors: ["#0fbcf9"],
+            tooltip: { y: { formatter: (value) => `$ ${value}` } },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
